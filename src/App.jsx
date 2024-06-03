9a4eff982d429de932553bfb9ec39d52140a3154
MariiaOtset <152022954+MariiaOtset@users.noreply.github.com>
2024-06-03 16:03:50 +0100

    components html added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
-import { useState } from 'react'
-import './App.css'
+// import { useState } from "react";
+import "./App.css";
+import Description from "./components/Description/Description";
+import Feedback from "./components/Feedback/Feedback";
+import Options from "./components/Options/Options";
 
 function App() {
+  // const [good, setGood] = useState(0);
+  // const [neutral, setNeutral] = useState(0);
+  // const [bad, setBad] = useState(0);
+  // const [feedbackTypes, setFeedbackTypes] = useState({
+  //   good: 0,
+  //   neutral: 0,
+  //   bad: 0,
+  // });
 
   return (
     <>
-      <h1>Vite + React</h1>
+      <Description />
+      <Options />
+      <Feedback />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
